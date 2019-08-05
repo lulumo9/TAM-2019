15cdbad3a205d552c95607f56dbe28acd80a35ac
Luisa Fernanda Munoz Ocampo <luisa_fernanda_munoz_ocampo@epam.com>
2019-08-05 11:57:16 +0200

    Guessing game +bonus

--- src/GuessingGame/Guessing.java
+++ src/GuessingGame/Guessing.java
@@ -10,33 +10,39 @@ public class Guessing {
         Random random = new Random();
         int systemNumber = random.nextInt(50); //Estoy diciendo que del 0 al 50
 
-        Boolean userWon = false;
         Boolean playAgain = true;
-        Scanner scanner = new Scanner(System.in);
-//        Scanner scanner2 = new Scanner(System.in);
+        String inputAnswer = "";
 
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome!");
         System.out.println("Try to guess the number that system has selected randomly!, Enter a number from 0 to 50: ");
 
 
         while (playAgain == true) {
-            while (userWon == false) {
 
-                int inputNumber = scanner.nextInt();
+            int inputNumber = scanner.nextInt();
 
-                if (inputNumber < systemNumber) {
-                    System.out.println("Try again, my number is bigger!");
-                } else if (inputNumber > systemNumber) {
-                    System.out.println("Try again my number is smaller!");
-                } else {
-                    System.out.println("You win - Bullseye!");
-                    System.out.println("Do you wanna play again? Type Y/N to answer: ");
-//                    playAgain = scanner2.next();     //Garantizo que solo tome el primer caracter
+            if (inputNumber < systemNumber) {
+                System.out.println("Try again, my number is bigger!");
+            } else if (inputNumber > systemNumber) {
+                System.out.println("Try again my number is smaller!");
+            } else {
+                System.out.println("You win - Bullseye!");
+                System.out.println(" ");
+                System.out.println("Do you wanna play again? Type Y/N to answer: ");
+                inputAnswer = scanner.next();
+                if (inputAnswer.equalsIgnoreCase("N")) {
+                    System.out.println("See you next time!");
+                    playAgain = false;
                 }
             }
         }
     }
 }
+
+
+
 
 
